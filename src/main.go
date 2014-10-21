2507ebf130acf7c6497004f1812b8e9d07ddc415
liuyanghejerry <liuyanghejerry@126.com>
2014-10-22 01:18:59 +0800

    实现简单的服务重启
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

--- src/main.go
+++ src/main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"RoomManager"
+	"errors"
 	"log"
 )
 
+func runServer() (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Server is down:", r)
+			err = errors.New("Server is down")
+		}
+	}()
+
+	var manager = RoomManager.ServeManager()
+	manager.Run()
+	return nil
+}
+
 func main() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	var manager = RoomManager.ServeManager()
-	log.Println(manager.Run())
+	for re, times := runServer(), 0; re != nil && times < 20; times++ {
+		re = runServer()
+	}
 	return
 }
