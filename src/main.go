ba4817d08b28fbe169094d1744dfdb74c729a073
liuyanghejerry <liuyanghejerry@126.com>
2014-11-02 11:16:41 +0800

    增加调试参数； 去除无意义的守护方案

--- src/main.go
+++ src/main.go
@@ -2,37 +2,23 @@ package main
 
 import (
 	"RoomManager"
-	"errors"
 	"log"
+	"runtime"
 )
 
 import _ "net/http/pprof"
 import http "net/http"
 
 func init() {
+	runtime.SetBlockProfileRate(1)
 	go func() {
 		log.Println(http.ListenAndServe("localhost:6767", nil))
 	}()
 }
 
-func runServer() (err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Println("Server is down:", r)
-			err = errors.New("Server is down")
-		}
-	}()
-
-	var manager = RoomManager.ServeManager()
-	manager.Run()
-	return nil
-}
-
 func main() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	for re, times := runServer(), 0; re != nil && times < 20; times++ {
-		re = runServer()
-	}
-	log.Println("Server died.")
+	var manager = RoomManager.ServeManager()
+	manager.Run()
 	return
 }
