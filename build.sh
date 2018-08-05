772f8c090bbf0cbb4e32b9227e1b6bc5c7d6ea40
liuyanghejerry <liuyanghejerry@126.com>
2018-08-05 14:19:53 +0800

    调整GOPATH

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-export GOPATH=`pwd`
-go build -o ./bin/painttyServer ./src/server/painttyServer.go
-go build -o ./bin/watchDog ./src/watchDog/watchDog.go
+GOPATH=`pwd` go build -o ./bin/painttyServer ./src/server/painttyServer.go
+GOPATH=`pwd` go build -o ./bin/watchDog ./src/watchDog/watchDog.go
